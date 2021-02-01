4de1d4370dc10a9d9fd5a56d3ec24ba9a585491b
Adiel Ribeiro <contato@nuvym.com>
2021-02-01 15:15:55 -0300

    first

---
+++ config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = '127.0.0.1';
+   public $dbuser     = 'glpi';
+   public $dbpassword = 'P%40ssw0rd-%23CHANGEME%21';
+   public $dbdefault  = 'glpi';
+}
